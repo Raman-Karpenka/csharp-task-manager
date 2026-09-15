76408ba1ebd941a76b197cd6177ab0c1838809fd
Raman-Karpenka <karpenka.raman@gmail.com>
2026-09-15 22:09:01 +0200

    Handle database update conflicts

--- TaskManager.Api/Handlers/GlobalExceptionHandler.cs
+++ TaskManager.Api/Handlers/GlobalExceptionHandler.cs
@@ -1,16 +1,17 @@
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
-
     private readonly ILogger<GlobalExceptionHandler> _logger;
 
     public GlobalExceptionHandler(
-    ILogger<GlobalExceptionHandler> logger)
+        ILogger<GlobalExceptionHandler> logger)
     {
         _logger = logger;
     }
+
     public async ValueTask<bool> TryHandleAsync(
         HttpContext httpContext,
         Exception exception,
@@ -19,6 +20,26 @@ public class GlobalExceptionHandler : IExceptionHandler
         _logger.LogError(
             exception,
             "Unhandled exception occurred.");
+
+        if (exception is DbUpdateException)
+        {
+            httpContext.Response.StatusCode =
+                StatusCodes.Status409Conflict;
+
+            httpContext.Response.ContentType =
+                "application/problem+json";
+
+            await httpContext.Response.WriteAsJsonAsync(
+                new ProblemDetails
+                {
+                    Status = StatusCodes.Status409Conflict,
+                    Title = "Conflict",
+                    Detail = "The operation conflicts with the current state of the data."
+                },
+                cancellationToken);
+
+            return true;
+        }
 
         httpContext.Response.StatusCode =
             StatusCodes.Status500InternalServerError;
